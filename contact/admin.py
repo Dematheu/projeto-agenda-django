1fa8542ed386714519724b0cf84a8bfe638d0534
Lucas de Matheu <125404264+Dematheu@users.noreply.github.com>
2023-05-23 12:49:43 -0300

    Feature/admin branch (#2)
    * Creating superuser
    
    * New feature created: contact model
    
    * Creating category
    
    * segundo dia de desenvolvimento
    
    * Third Day: Heads, forms, pagination and more.
    
    * First forms configs
    
    * Form Configurations at 90%
    
    * Finished form configuration

--- contact/admin.py
+++ contact/admin.py
@@ -4,14 +4,22 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'show',)
     list_display = ('id', 'first_name', 'last_name', 'phone',)
+
     ordering = '-id',
     # list_filter = ('created_date',)
     search_fields = ('id', 'first_name', 'last_name',)
     list_per_page = 10
     list_max_show_all = 200
+
+    list_editable = 'first_name', 'last_name', 'show'
+    list_display_links = 'id',
+
     # list_editable = 'first_name', 'last_name'
     list_display_links = 'id', 'first_name'
+
 
 
 @admin.register(models.Category)
